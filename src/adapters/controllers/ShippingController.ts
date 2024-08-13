f739490dce48f9489a860d7e91cee822edb44ecd
Daniel Braz <danielbrazlopes16@gmail.com>
2024-08-12 22:34:20 -0300

    feat(main): implement get address shipping controller method

--- src/adapters/controllers/ShippingController.ts
+++ src/adapters/controllers/ShippingController.ts
@@ -2,16 +2,33 @@ import { inject, injectable } from "inversify";
 import { HealthDto } from "adapters/dtos/HealthDto";
 import { IHttpStatus } from "adapters/interfaces/IHttpStatus";
 import { IShippingController } from "adapters/interfaces/IShippingController";
+import { GetAddressDto } from "adapters/dtos/GetAddressDto";
+import { IGetAddressUseCase } from "application/interfaces/IGetAddressUseCase";
+import { AddressDto } from "application/dtos/AddressDto";
 
 @injectable()
 export class ShippingController implements IShippingController {
   private readonly httpStatus: IHttpStatus;
+  private readonly getAddressUseCase: IGetAddressUseCase;
 
-  constructor(@inject('IHttpStatus') httpStatus: IHttpStatus) {
-    this.httpStatus = httpStatus
+  constructor(
+    @inject('IHttpStatus') httpStatus: IHttpStatus,
+    @inject('IGetAddressUseCase') getAddressUseCase: IGetAddressUseCase,
+  ) {
+    this.httpStatus = httpStatus;
+    this.getAddressUseCase = getAddressUseCase;
   }
 
   public health(): HealthDto {
     return new HealthDto(this.httpStatus.OK);
+  }
+
+  public async getAddress(zipCode: string): Promise<GetAddressDto> {
+    const addressDto: AddressDto = await this.getAddressUseCase.execute(zipCode);
+    const getAddressDto: GetAddressDto = new GetAddressDto(
+      this.httpStatus.OK,
+      addressDto,
+    );
+    return getAddressDto;
   }
 }
--- src/adapters/interfaces/IShippingController.ts
+++ src/adapters/interfaces/IShippingController.ts
@@ -1,5 +1,7 @@
 import { HealthDto } from "adapters/dtos/HealthDto";
+import { GetAddressDto } from "adapters/dtos/GetAddressDto";
 
 export interface IShippingController {
   health(): HealthDto;
+  getAddress(zipCode: string): Promise<GetAddressDto>;
 }
